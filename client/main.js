bbc797272fdbf21d61fecbe5a22e9c90d2ae78a2
Derik McCausland <derikmccausland@gmail.com>
2022-08-02 11:37:53 -0600

    new computer

---
+++ client/main.js
@@ -0,0 +1,112 @@
+const moviesContainer = document.querySelector('#movies-container')
+const form = document.querySelector('form')
+
+const baseURL = `http://localhost:4000/api/movies`
+
+const moviesCallback = ({ data: movies }) => displayMovies(movies)
+const errCallback = err => console.log(err.response.data)
+
+const getAllMovies = () => axios.get(baseURL).then(moviesCallback).catch(errCallback)
+const createMovie = body => axios.post(baseURL, body).then(moviesCallback).catch(errCallback)
+const deleteMovie = id => axios.delete(`${baseURL}/${id}`).then(moviesCallback).catch(errCallback)
+
+function submitHandler(e) {
+    e.preventDefault()
+
+    let title = document.querySelector('#title')
+    let spellLvl = document.querySelector('input[name="ratings"]:checked')
+
+    let bodyObj = {
+        title: title.value,
+        spellLvl: spellLvl.value, 
+    }
+
+    createMovie(bodyObj)
+
+    title.value = ''
+    spellLvl.value = false
+}
+
+function createMovieCard(movie) {
+    const movieCard = document.createElement('div')
+    movieCard.classList.add('movie-card')
+
+    movieCard.innerHTML = `
+    <button onclick="castSpell(${movie.spellLvl})">Cast Spell</button>
+    <p class="movie-title">${movie.title}</p>
+    <div class="btns-container">
+    
+        <p class="movie-spellLvl">${movie.spellLvl}</p>
+        
+    </div>
+    <button onclick="deleteMovie(${movie.id})">Remove Spell</button>
+    `
+
+
+    moviesContainer.appendChild(movieCard)
+}
+
+function displayMovies(arr) {
+    let arr2 = arr.sort(function(a, b) {
+        return a.spellLvl - b.spellLvl
+    })
+    moviesContainer.innerHTML = ``
+    for (let i = 0; i < arr2.length; i++) {
+        createMovieCard(arr[i])
+    }
+}
+
+function castSpell(lvl) {
+    if(lvl === 0) {
+        alert(`You have cast a level ${lvl} spell!`)
+    } 
+     else if (lvl === 1) {
+        if (document.getElementById('spelslot1').checked === false) {
+            document.getElementById('spelslot1').checked = true
+            alert(`You have cast a level ${lvl} spell!`)
+    } else if (document.getElementById('spelslot2').checked === false) {
+        document.getElementById('spelslot2').checked = true
+        alert(`You have cast a level ${lvl} spell!`)
+    }  else if (document.getElementById('spelslot3').checked === false) {
+        document.getElementById('spelslot3').checked = true
+        alert(`You have cast a level ${lvl} spell!`)
+    }  else if (document.getElementById('spelslot4').checked === false) {
+        document.getElementById('spelslot4').checked = true
+        alert(`You have cast a level ${lvl} spell!`)
+    } else {
+        alert(`You don't have the slots to cast a level ${lvl} spell!`)
+    }
+}
+    else if (lvl === 2) {
+        if (document.getElementById('spelslot5').checked === false) {
+            document.getElementById('spelslot5').checked = true
+            alert(`You have cast a level ${lvl} spell!`)
+        } else if (document.getElementById('spelslot6').checked === false) {
+            document.getElementById('spelslot6').checked = true
+            alert(`You have cast a level ${lvl} spell!`)
+        }  else if (document.getElementById('spelslot7').checked === false) {
+            document.getElementById('spelslot7').checked = true
+            alert(`You have cast a level ${lvl} spell!`)
+        } else {
+            alert(`You don't have the slots to cast a level ${lvl} spell!`)
+        }
+    }
+    else if (lvl === 3) {
+        if (document.getElementById('spelslot8').checked === false) {
+            document.getElementById('spelslot8').checked = true
+            alert(`You have cast a level ${lvl} spell!`)
+        } else if (document.getElementById('spelslot9').checked === false) {
+            document.getElementById('spelslot9').checked = true
+            alert(`You have cast a level ${lvl} spell!`)
+        } else {
+            alert(`You don't have the slots to cast a level ${lvl} spell!`)
+        }
+    } else {
+        alert(`You don't have the slots to cast a level ${lvl} spell!`)
+    }
+
+}
+
+form.addEventListener('submit', submitHandler)
+
+getAllMovies()
---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 4000
+const app = express();
+const path = require("path");
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+
+app.get('/css', (req, res) =>{
+    res.sendFile(path.join(__dirname, '../client/styles.css'))
+})
+
+app.get('/js', (req, res) =>{
+    res.sendFile(path.join(__dirname, '../client/main.js'))
+})
+
+const {getMovies, deleteMovie, createMovie, updateMovie} = require('./controller.js')
+
+app.get(`/api/movies`, getMovies)
+app.delete(`/api/movies/:id`, deleteMovie)
+app.post(`/api/movies`, createMovie)
+app.put(`/api/movies/:id`, updateMovie)
+
+app.listen(port, () => console.log("Server running on 4000"));
+
